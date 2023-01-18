727223824683af4abcd37e1e1b38de670fcc63e9
poopsicles <87488715+poopsicles@users.noreply.github.com>
2023-01-18 22:36:50 +0100

    stylised stuff

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
 - [ ] add calculator
 - [ ] remove placeholder names and pictures
-- [ ] add matric. num., fave food etc.
+- [X] add matric. num., fave food etc.
 - [X] allow adding of new persons
 
 ## styling
 
-- [ ] change page background to dark
-- [ ] change font to [Inter](https://rsms.me/inter/)
+- [X] change page background to dark
+- [X] change font to [Inter](https://rsms.me/inter/)
 - [ ] make it look nice
--- script.js
+++ script.js
@@ -1,37 +1,68 @@
 function addPerson() {
-    // Get the people grid element
-    const peopleGrid = document.querySelector('.people-grid');
-
     // Get the form input values
-    const name = document.getElementById('name').value;
-    const age = document.getElementById('age').value;
-
-    // Read the image's base64 data
-    const file_reader = new FileReader();
-    file_reader.onload = function () {
-        newPersonImage.src = file_reader.result; // await the file data
+    const name = document.querySelector('#name').value;
+    const matric = document.querySelector('#matric').value;
+    const age = document.querySelector('#age').value;
+    const food = document.querySelector('#food').value;
+    const image = document.querySelector('#image').files[0];
+  
+    // Check if the form fields are empty
+    if (!name || !matric || !age || !food || !image) {
+      alert("Please fill in all the fields.");
+      return;
     }
-    file_reader.readAsDataURL(document.getElementById('image').files[0]); // asynchronously get the data
+  
+    // Create a new FileReader object
+    const reader = new FileReader();
+  
+    // Add an event listener for when the file has been read
+    reader.addEventListener('load', () => {
+      // Get the people grid element
+      const peopleGrid = document.querySelector('.people-grid');
+  
+      // Create the new person
+      const newPerson = document.createElement('div');
+      newPerson.classList.add('person');
+  
+      // Create the image element
+      const newPersonImage = document.createElement('img');
+      newPersonImage.src = reader.result;
+      newPersonImage.alt = name;
+      newPerson.appendChild(newPersonImage);
+  
+      // Create the name element
+      const newPersonName = document.createElement('h3');
+      newPersonName.innerHTML = `${name}`;
+      newPerson.appendChild(newPersonName);
+  
+      // Create the matric element
+      const newPersonMatric = document.createElement('p');
+      newPersonMatric.innerHTML = `${matric}`;
+      newPerson.appendChild(newPersonMatric);
 
-    // Create the new person element
-    const newPerson = document.createElement('div');
-    newPerson.classList.add('person');
+      // Create the age element
+      const newPersonAge = document.createElement('p');
+      newPersonAge.classList.add('thin');
+      newPersonAge.innerHTML = `${age} years old`;
+      newPerson.appendChild(newPersonAge);
 
-    // Create the image element
-    const newPersonImage = document.createElement('img');
-    newPersonImage.alt = name;
-    newPerson.appendChild(newPersonImage);
-
-    // Create the name element
-    const newPersonName = document.createElement('h3');
-    newPersonName.innerHTML = `Name: ${name}`;
-    newPerson.appendChild(newPersonName);
-
-    // Create the age element
-    const newPersonAge = document.createElement('p');
-    newPersonAge.innerHTML = `Age: ${age}`;
-    newPerson.appendChild(newPersonAge);
-    
-    // Add the new person to the people grid
-    peopleGrid.appendChild(newPerson);
-}
+      // Create the food element
+      const newPersonFood = document.createElement('p');
+      newPersonFood.classList.add('light');
+      newPersonFood.innerHTML = `<br>${food}`;
+      newPerson.appendChild(newPersonFood);
+  
+      // Add the new person to the people grid
+      peopleGrid.appendChild(newPerson);
+  
+      // Get the form element
+      const form = document.querySelector('form');
+  
+      // Reset the form fields
+      form.reset();
+    });
+  
+    // Read the file as a data URL
+    reader.readAsDataURL(image);
+  }
+  
